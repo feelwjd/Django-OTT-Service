1502c3df5d67209773e24c18cccf86b1650732ee
feelwjd <feelwjd@gmail.com>
2020-10-22 23:13:16 +0900

    video

--- project/streamming/models.py
+++ project/streamming/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
-
 # Create your models here.
+
+class StreammingNvideo(models.Model):
+    nvideo = models.FileField(upload_to='videos/',null=True,verbose_name="")
+    nid = models.IntegerField(primary_key=True)
+    
+    class Meta:
+        managed = False
+        db_table = 'streamming_nvideo' 
--- project/streamming/views.py
+++ project/streamming/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import StreammingNvideo
+from .forms import VideoForm
 # Create your views here.
+def video(request):
+    nv = StreammingNvideo.objects.last()
+    nvideos = nv.nvideo
+
+    
+     
+    return render(request, 'video.html',{'nvideos':nvideos})
