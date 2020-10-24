298f7e066a40dbd1b5bc05bdabba237c5cd904fd
KimJaei <rse178@naver.com>
2020-10-24 22:44:58 +0900

    연결

--- project/streamming/urls.py
+++ project/streamming/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from . import views
+import streamming.views
 
 urlpatterns = [
-    
+    path('', streamming.views.index, name='index'),
+    path('book/', streamming.views.book, name='book'),
+    path('book2/', streamming.views.book2, name='book2'),
+    path('book3/', streamming.views.book3, name='book3'),
+    path('book4/', streamming.views.book4, name='book4'),
+    path('book5/', streamming.views.book5, name='book5'),
+    path('detail/', streamming.views.detail, name='detail1'),
+    path('detail2/', streamming.views.detail2, name='detila2'),
+    path('detail3/', streamming.views.detail3, name='detail3'),
+    path('detail4/', streamming.views.detail4, name='detail4'),
+    path('detail5/', streamming.views.detail5, name='detail5'),
 ]
--- project/streamming/views.py
+++ project/streamming/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def book(request):
+    return render(request, 'book.html')
+
+def book2(request):
+    return render(request, 'book2.html')
+
+def book3(request):
+    return render(request, 'book3.html')
+
+def book4(request):
+    return render(request, 'book4.html')
+
+def book5(request):
+    return render(request, 'book5.html')
+
+def detail(request):
+    return render(request, 'detail.html')
+
+def detail2(request):
+    return render(request, 'detail2.html')
+
+def detail3(request):
+    return render(request, 'detail3.html')
+
+def detail4(request):
+    return render(request, 'detail4.html')
+
+def detail5(request):
+    return render(request, 'detail5.html')
